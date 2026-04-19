c89c13e488e6a856d46a7dee38c0abf036e63c87
Ramiro Cordero <corderoramiro14@gmail.com>
2026-04-19 17:53:12 -0300

    Prueba!

---
+++ Proyecto-Ing.-Software/Prueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Ing._Software
+{
+    internal class Prueba
+    {
+    }
+}
