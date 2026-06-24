de015eccbe5dd1c31569b700bbc3080ff970a69a
Lucas-contrib <maileducacion@protonmail.com>
2026-06-23 23:50:24 -0300

    string de conexión para desarrollo agregado

--- DAL/ConexionBD.cs
+++ DAL/ConexionBD.cs
@@ -11,8 +11,10 @@ namespace DAL
     public class ConexionBD
     {
         //Cadena que conecta la base de datos con el codigo
-        string cadenaConexion = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Fulbito500;Integrated Security=True;";
-    
+        // string cadenaConexion = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Fulbito500;Integrated Security=True;";
+        // cadena para máquina de Lucas, descomentar si es necesario
+        string cadenaConexion = @"Data Source=localhost\SQLEXPRESS01;Initial Catalog=Fulbito500;Integrated Security=True;";
+
         //Metodo que valida la conexion con la base de datos
         public SqlConnection ValidarConexion()
         {
